8a81af28ab2a3d415dc9ce62a36a837f42cabac2
Sean Cobham <cobhamsean@gmail.com>
2022-11-20 16:06:29 -0800

    Tutorial 7 completed

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from email.policy import default
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 
@@ -13,6 +14,11 @@ class Question(models.Model):
     def __str__(self) -> str:
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
